a4fca56a2dd0d53af503ff4ee229cb8fd89e5da3
Hiếu Lê <hieule@192.168.1.14>
2026-02-05 00:59:03 +0700

    first initial project struture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:task_taste/main.dart';
+import 'package:task_taste/task_taste.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const TaskTaste());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
